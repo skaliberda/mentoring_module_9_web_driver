3a15b0fb283bfe2355c762d81c2720e7099b646b
olyv <a.n.lyvin@gmail.com>
2014-02-14 18:10:42 +0200

    corrected Builder, initialisation of Builder moved into CreateLetterDraftTest

--- src/main/java/com/epam/mentoring/core/TestData.java
+++ src/main/java/com/epam/mentoring/core/TestData.java
@@ -2,19 +2,25 @@ package com.epam.mentoring.core;
 
 public class TestData {
 	
-	public String letterAddressee = "";
-	public String letterSubject = "";
-	public String letterBody = "";
+	private String letterAddressee;
+	private String letterSubject;
+	private String letterBody;
+
+	public TestData(String letterAddressee, String letterSubject, String letterBody) {
+		this.letterAddressee = letterAddressee;
+		this.letterSubject = letterSubject;
+		this.letterBody = letterBody;
+	 }
 	
 	public String getAddressee() {
 		return this.letterAddressee;
 	}
-	
+
 	public String getSubject() {
 		return this.letterSubject;
 	}
-	
+
 	public String getBody() {
 		return this.letterBody;
-	}
+	}	
 }
